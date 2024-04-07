5c27860d4044464f9bc685e12a67ec9dc09c2248
morin.ia <MorinIA.sale@relef.ru>
2024-04-07 22:11:35 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TestTaskExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonPauseContinue = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelTotalFiles = new System.Windows.Forms.Label();
+            this.labelFoundFiles = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelCurrentFolder = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxRegex = new System.Windows.Forms.TextBox();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 108);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(903, 306);
+            this.treeView1.TabIndex = 0;
+            // 
+            // buttonSelectFolder
+            // 
+            this.buttonSelectFolder.Location = new System.Drawing.Point(296, 34);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectFolder.TabIndex = 2;
+            this.buttonSelectFolder.Text = "Обзор";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(12, 63);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(562, 23);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonPauseContinue
+            // 
+            this.buttonPauseContinue.Location = new System.Drawing.Point(580, 34);
+            this.buttonPauseContinue.Name = "buttonPauseContinue";
+            this.buttonPauseContinue.Size = new System.Drawing.Size(146, 23);
+            this.buttonPauseContinue.TabIndex = 2;
+            this.buttonPauseContinue.Text = "Пауза";
+            this.buttonPauseContinue.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(580, 63);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(146, 23);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Сброс";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(847, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Найдено:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(813, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Всего файлов:";
+            // 
+            // labelTotalFiles
+            // 
+            this.labelTotalFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTotalFiles.Location = new System.Drawing.Point(732, 25);
+            this.labelTotalFiles.Name = "labelTotalFiles";
+            this.labelTotalFiles.Size = new System.Drawing.Size(183, 16);
+            this.labelTotalFiles.TabIndex = 3;
+            this.labelTotalFiles.Text = "0";
+            this.labelTotalFiles.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelFoundFiles
+            // 
+            this.labelFoundFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFoundFiles.Location = new System.Drawing.Point(732, 57);
+            this.labelFoundFiles.Name = "labelFoundFiles";
+            this.labelFoundFiles.Size = new System.Drawing.Size(183, 16);
+            this.labelFoundFiles.TabIndex = 3;
+            this.labelFoundFiles.Text = "0";
+            this.labelFoundFiles.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Поиск в";
+            // 
+            // labelCurrentFolder
+            // 
+            this.labelCurrentFolder.AutoSize = true;
+            this.labelCurrentFolder.Location = new System.Drawing.Point(72, 89);
+            this.labelCurrentFolder.Name = "labelCurrentFolder";
+            this.labelCurrentFolder.Size = new System.Drawing.Size(11, 16);
+            this.labelCurrentFolder.TabIndex = 4;
+            this.labelCurrentFolder.Text = "-";
+            this.labelCurrentFolder.Click += new System.EventHandler(this.labelCurrentFolder_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Начальный каталог";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(374, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Регулярное выражение";
+            // 
+            // textBoxRegex
+            // 
+            this.textBoxRegex.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::TestTaskExplorer.Properties.Settings.Default, "Regex", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBoxRegex.Location = new System.Drawing.Point(377, 34);
+            this.textBoxRegex.Name = "textBoxRegex";
+            this.textBoxRegex.Size = new System.Drawing.Size(197, 22);
+            this.textBoxRegex.TabIndex = 1;
+            this.textBoxRegex.Text = global::TestTaskExplorer.Properties.Settings.Default.Regex;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::TestTaskExplorer.Properties.Settings.Default, "SearchPath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBoxPath.Location = new System.Drawing.Point(12, 34);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(278, 22);
+            this.textBoxPath.TabIndex = 1;
+            this.textBoxPath.Text = global::TestTaskExplorer.Properties.Settings.Default.SearchPath;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 426);
+            this.Controls.Add(this.labelCurrentFolder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelFoundFiles);
+            this.Controls.Add(this.labelTotalFiles);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonPauseContinue);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.Controls.Add(this.textBoxRegex);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.treeView1);
+            this.MinimumSize = new System.Drawing.Size(945, 320);
+            this.Name = "Form1";
+            this.Text = "Поиск файлов по регулярному выражению";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.TextBox textBoxRegex;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonPauseContinue;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelTotalFiles;
+        private System.Windows.Forms.Label labelFoundFiles;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelCurrentFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestTaskExplorer.Properties;
+
+namespace TestTaskExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelCurrentFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSelectFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Save();
+        }
+    }
+}
